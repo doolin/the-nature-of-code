0f4d0188b6990e73d45dd9bedc7758467ef4cde4
dave doolin <david.doolin@gmail.com>
2024-05-22 05:52:23 -0700

    drawing a hexagon

--- js/hexagon.js
+++ js/hexagon.js
@@ -9,6 +9,21 @@ const hexagon = (sketch) => {
     canvas.parent('hexagons');
     sketch.background(sketch.background_color);
   }
+
+  sketch.draw = () => {
+    sketch.background(sketch.background_color);
+    sketch.rotateX(sketch.frameCount * 0.01);
+    sketch.rotateY(sketch.frameCount * 0.01);
+    sketch.stroke(0);
+    sketch.fill(0, 100, 0);
+    sketch.beginShape();
+    for (let i = 0; i < 2 * Math.PI; i += Math.PI / 3) {
+      let x = 100 * Math.cos(i);
+      let y = 100 * Math.sin(i);
+      sketch.vertex(x, y);
+    }
+    sketch.endShape(sketch.CLOSE);
+  }
 }
 
 let hexDraw = new p5(hexagon);
