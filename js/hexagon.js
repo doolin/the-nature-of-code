623a372698b9415fa1fe8932e42e3d4fb3377a61
dave doolin <david.doolin@gmail.com>
2024-05-23 04:42:35 -0700

    now drawing two hexes

--- js/hexagon.js
+++ js/hexagon.js
@@ -10,19 +10,27 @@ const hexagon = (sketch) => {
     sketch.background(sketch.background_color);
   }
 
-  sketch.draw = () => {
-    sketch.background(sketch.background_color);
-    sketch.rotateX(sketch.frameCount * 0.01);
-    sketch.rotateY(sketch.frameCount * 0.01);
-    sketch.stroke(0);
-    sketch.fill(0, 100, 0);
+  sketch.drawHexagon = (x_0, y_0) => {
     sketch.beginShape();
     for (let i = 0; i < 2 * Math.PI; i += Math.PI / 3) {
-      let x = 100 * Math.cos(i);
-      let y = 100 * Math.sin(i);
+      let x = x_0 * Math.cos(i);
+      let y = y_0 * Math.sin(i);
       sketch.vertex(x, y);
     }
     sketch.endShape(sketch.CLOSE);
+  }
+
+  sketch.draw = () => {
+    let radius = 50
+    sketch.background(sketch.background_color);
+    // sketch.rotateX(sketch.frameCount * 0.01);
+    // sketch.rotateY(sketch.frameCount * 0.01);
+    sketch.stroke(255);
+    sketch.strokeWeight(2);
+    sketch.fill(0, 100, 0);
+    sketch.drawHexagon(radius, radius);
+    sketch.translate(50, 50, 0);
+    sketch.drawHexagon(radius, radius);
   }
 }
 
