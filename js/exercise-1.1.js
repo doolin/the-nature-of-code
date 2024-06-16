f1fb98211c23059e6b99b520536c8dccaf56a645
dave doolin <david.doolin@gmail.com>
2024-06-16 05:00:05 -0700

    abstracted canvas wrapping code

--- js/exercise-1.1.js
+++ js/exercise-1.1.js
@@ -31,9 +31,25 @@
       stroke([250, 0, 100]);
       point(this.o);
       stroke([0, 0, 150]);
-      // point(this.p);
-      // stroke([0, 200, 0]);
-      point(p5.Vector.rotate(this.o.sub(this.midpoint).add(this.midpoint), 0.3))
+      point(this.p);
+      stroke([0, 200, 0]);
+      // point(p5.Vector.rotate(this.o.sub(this.midpoint).add(this.midpoint), 0.3))
+    }
+
+    wrap(point) {
+      // Wrap x around
+      if (point.x >= width) {
+        point.x = point.x % width;
+      } else if (point.x < 0) {
+        point.x = (point.x % width) + width;
+      }
+
+      // Wrap y around
+      if (point.y >= height) {
+        point.y = point.y % height;
+      } else if (point.y < 0) {
+        point.y = (point.y % height) + height;
+      }
     }
 
     step() {
@@ -52,33 +68,8 @@
           this.p.x--;
       }
 
-      // Wrap x around
-      if (this.o.x >= width) {
-        this.o.x = this.x % width;
-      } else if (this.o.x < 0) {
-        this.o.x = (this.o.x % width) + width;
-      }
-
-      // Wrap y around
-      if (this.o.y >= height) {
-        this.o.y = this.o.y % height;
-      } else if (this.o.y < 0) {
-        this.o.y = (this.o.y % height) + height;
-      }
-
-      // Wrap p.x around
-      if (this.p.x >= width) {
-        this.p.x = this.p.x % width;
-      } else if (this.p.x < 0) {
-        this.p.x = (this.p.x % width) + width;
-      }
-
-      // Wrap p.y around
-      if (this.p.y >= height) {
-        this.p.y = this.p.y % height;
-      } else if (this.p.y < 0) {
-        this.p.y = (this.p.y % height) + height;
-      }
+      this.wrap(this.o);
+      this.wrap(this.p);
     }
   }
 
