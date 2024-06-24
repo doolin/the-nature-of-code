9828b8d474e8993bccdfe19a25b23378ed6c7a24
dave doolin <david.doolin@gmail.com>
2024-06-24 05:03:42 -0700

    hex splat fading in

--- js/splat.js
+++ js/splat.js
@@ -1,5 +1,7 @@
 const splat = (sketch) => {
   sketch.background_color = [245, 245, 220];
+  let opacity = 0;
+  let ostep = 2;
 
   let width = 480;
   let height = 200;
@@ -18,25 +20,28 @@ const splat = (sketch) => {
       sketch.vertex(x, y);
     }
     sketch.endShape(sketch.CLOSE);
-    sketch.fill(200,0,0);
-    sketch.circle(x_0, y_0, 20);
+
+    // an associated circle.
+    // sketch.fill(200,0,0);
+    // sketch.circle(x_0, y_0, 20);
   }
 
   sketch.draw = () => {
     let radius = 50
-    sketch.frameRate(1);
+    sketch.frameRate(30);
     sketch.background(sketch.background_color);
     // sketch.rotateX(sketch.frameCount * 0.01);
     // sketch.rotateY(sketch.frameCount * 0.01);
     sketch.stroke(255);
     sketch.strokeWeight(2);
-    sketch.fill(0, 100, 0);
+    sketch.fill(0, 100, 0, opacity);
     // sketch.drawSplat(radius, radius);
     // sketch.translate(50, 50, 0);
-    x = random(width);
-    y = random(height);
+    x = width / 2; // random(width);
+    y = height / 2; // random(height);
     sketch.translate(x, y);
     sketch.drawSplat(radius, radius);
+    opacity = (opacity + ostep) % 255;
   }
 }
 
